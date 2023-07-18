1fc54561012931b760f1b96eaf72086e7b81c834
elysthetruechi <kaluelizabeth29@gmail.com>
2023-07-18 12:23:32 +0100

    .

---
+++ script.js
@@ -0,0 +1,25 @@
+$('.carousel').owlCarousel({
+    margin:0,
+    loop:false,
+    autoplayTimeOut:0,
+    autoplayHoverPause:false,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        600:{
+            items: 1,
+            nav: false
+        },
+        0:{
+            items: 1,
+            nav: false
+        }
+    }
+});
+
+$('.next-btn').click(function(){
+    $('.next-btn').toggleClass("next-btnActive");
+    
+});
